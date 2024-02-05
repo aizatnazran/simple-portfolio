cbcf0b19921a81938f94e05175bd61183e299fde
aizatnazran <aizatnazran@gmail.com>
2024-02-05 19:55:55 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Vue Tailwind Portfolio
+
+Minimal portfolio made on VUE JS and Tailwind CSS.
+
+## Features
+
+- Light/dark mode toggle
+- Fetched Projects from github repositories
+- Mobile responsive
+
+## Run Locally
+
+Go to the project directory
+
+```bash
+  cd my-portfolio
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run dev
+```
+
+Build the project
+
+```bash
+  npm run build
+```
---
+++ src/components/CardFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <hr class="border-1 my-8 border-gray-400 sm:w-4/12 mx-auto" />
+    <div
+      class="text-center my-4 dark:text-white flex items-center justify-center"
+    >
+      Contact me via:
+    </div>
+    <div class="text-center">
+      <a
+        href="https://www.linkedin.com/in/aizat-nazran/"
+        target="_blank"
+        class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"
+        ><font-awesome-icon icon="fa-brands fa-linkedin"
+      /></a>
+      <a
+        href="https://github.com/aizatnazran"
+        target="_blank"
+        class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"
+      >
+        <font-awesome-icon icon="fa-brands fa-github"
+      /></a>
+      <a
+        href="mailto:aizatnazran@gmail.com"
+        class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"
+      >
+        <font-awesome-icon icon="fa-solid fa-envelope"
+      /></a>
+    </div>
+  </div>
+</template>
+<script setup></script>
